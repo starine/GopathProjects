a462b3565af7beb6b7158e4467f4277de5aaff5f
MandarinOrange <istars0110@gmail.com>
2021-09-30 10:34:47 +0800

    map 的遍历以及delete

--- src/day003/map/map_test.go
+++ src/day003/map/map_test.go
@@ -1,7 +1,14 @@
 package map_test
 
-import "testing"
+import (
+	"testing"
+)
 
+/*
+Map 是一种无序的键值对的集合。Map 最重要的一点是通过 key 来快速检索数据，key 类似于索引，指向数据的值。
+Map 是一种集合，所以我们可以像迭代数组和切片那样迭代它。不过，Map 是无序的，我们无法决定它的返回顺序，
+这是因为 Map 是使用 hash 表来实现的。
+*/
 func TestInitMap(t *testing.T) {
 	m1 := map[int]int{1: 2, 2: 4, 3: 8}
 	t.Log(m1[2])
@@ -44,5 +51,34 @@ func TestMapTravel(t *testing.T) {
 	m5 := map[int]int{1: 3, 2: 9, 3: 27, 4: 81}
 	for k, v := range m5 {
 		t.Logf("key:%d, value:%d", k, v)
+	}
+}
+
+//第二种遍历方式
+func TestMapTravel2(t *testing.T) {
+	m5 := map[int]int{1: 3, 2: 9, 3: 27, 4: 81}
+	for k := range m5 {
+		t.Logf("key:%d, value:%d", k, m5[k])
+	}
+}
+
+//delete（）函数用于删除集合的元素， 参数为 map 和其对应的 key。
+func TestDeleteMap(t *testing.T) {
+	//创建map
+	countryCapitalMap := map[string]string{"France": "Paris", "Italy": "Rome", "Japan": "Tokyo", "India": "New delhi"}
+
+	t.Log("原始地图")
+
+	//打印地图
+	for country := range countryCapitalMap {
+		t.Log(country, "首都是", countryCapitalMap[country])
+	}
+	//删除元素
+	delete(countryCapitalMap, "France")
+	t.Log("法国条目被删除")
+	t.Log("删除后的地图")
+	//打印地图
+	for country := range countryCapitalMap {
+		t.Log(country, "首都是", countryCapitalMap[country])
 	}
 }
